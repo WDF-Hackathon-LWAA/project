308f104d19198533ac82321e401cbb774a8420c5
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2018-03-31 13:31:05 -0400

    add basic style

--- app/components/EmployerLanding.js
+++ app/components/EmployerLanding.js
@@ -4,16 +4,22 @@ import { Link } from 'react-router-dom';
 class EmployerLanding extends Component {
 
   state = {
+    employer: {
+      name: 'Janet'
+    },
     roleId: 1
   }
 
   render() {
 
     let { roleId } = this.state;
+    let { name } = this.state.employer;
 
     return (
-      <div>
-        <h2>Welcome back, Employer</h2>
+      <div className="employer-landing">
+        <h2>Welcome back, {name}</h2>
+        <br />
+        <h4>Your roles:</h4>
         <Link to={`/result/${roleId}`}><input type="submit" value="View a role" /></Link>
       </div>
     );
--- app/components/Role.js
+++ app/components/Role.js
@@ -3,17 +3,29 @@ import { Link } from 'react-router-dom';
 
 class Role extends Component {
 
-
+  state = {
+    candidates: ['bob', 'jane', 'whoever', 'someone']
+  }
 
   render() {
+
+    let { candidates } = this.state;
 
     return (
       <div>
         <h3>YO, this is your posted role!</h3>
+        <br />
+        <h4>Applied candidates</h4>
+        {
+          candidates.map((candidate, i) =>
+            <li key={i}>{candidate}</li>
+          )
+        }
+        <br />
         <Link to="/"><input type="submit" value="Go Back" /></Link>
       </div>
 
-    )
+    );
   }
 }
 
