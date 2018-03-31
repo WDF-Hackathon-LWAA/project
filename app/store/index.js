666e84c36bc006f49b8284415866ba0af1c33917
AnthonyClausing <AnthonyClausing@gmail.com>
2018-03-31 16:12:04 -0400

    created user reducer

--- app/store/index.js
+++ app/store/index.js
@@ -1,16 +1,13 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import user from './user';
 
 const middleware = applyMiddleware(thunkMiddleware);
 
-const reducer = combineReducers({
-  // photos,
-  // searchTerm
-});
+const reducer = combineReducers({user});
 
 const store = createStore(reducer, middleware);
 
 export default store;
 
-// export * from './photos';
-// export * from './searchTerm';
+export * from './user';
